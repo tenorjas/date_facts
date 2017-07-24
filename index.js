d1615c3e3e394da91aafcce964ee517383b3e6a8
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-07-24 15:24:35 -0400

    got DST

--- index.js
+++ index.js
@@ -20,3 +20,13 @@ console.log(
     chalk.rgb(98, 182, 145)(a.diff(b, "seconds")) +
     " seconds into the day."
 );
+
+if (moment().isDST()) {
+  console.log(
+    "It " + chalk.rgb(37, 124, 27)("is") + " during Daylight Savings Time."
+  );
+} else {
+  console.log(
+    "It " + chalk.rgb(37, 124, 27)("is not") + " during Daylight Savings Time."
+  );
+}
