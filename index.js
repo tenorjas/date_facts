4c49c53556184da6e6e9846688e5fff8ea163675
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-07-24 14:31:30 -0400

    finished display date line

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const chalk = require("chalk");
 const moment = require("moment");
 
-console.log(chalk.red("This should be red."));
+console.log(
+  "It is " +
+    chalk.rgb(105, 210, 207)(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+);
