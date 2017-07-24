70532c188132bac1b3cbd3f22bd549245318d00c
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-07-24 14:42:32 -0400

    got day of year

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ console.log(
   "It is " +
     chalk.rgb(105, 210, 207)(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
 );
+
+console.log(
+  "It is the " +
+    chalk.rgb(186, 81, 182)(moment().format("DDDo")) +
+    " day of the year."
+);
